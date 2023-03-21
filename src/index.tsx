fe1edf3f58cf45f66df0283e4507dc018205c778
Usov_Artem <artemka063@gmail.com>
2023-03-21 11:56:36 +0400

    add Bag section

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import BagGray from './Components/Bag';
 import Header from './Components/Header';
 import FeatureSection from './Components/FeatureSection';
 import Footer from './Components/Footer';
 import "../src/css/iconstyle.css"; 
-// import App from './App';
+
 
 
 
@@ -23,7 +22,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Header/>
-    {/* <App/> */}
     <BagGray/>
     <FeatureSection/>
     <Footer/>

9df764e09bd77f275b3b31d3ee7dab9578765205
Usov_Artem <artemka063@gmail.com>
2023-05-03 19:30:38 +0400

    Fonts update

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "../src/css/iconStyle.css";
-import "../src/css/iconStyleTwo.css";
-import "../src/css/iconStyleThree.css"; 
-import "../src/css/icon.css"
-import "../src/css/ionicons.min.css" 
+import "../src/css/iconStyleFontAwesome.css";
+import "../src/css/iconStylePeIcon7Stroke.css"; 
+import "../src/css/icon.css";
+import "../src/css/styleFredokaOneRegular.css";
+import "../src/css/ionicons.min.css";
+// import "../src/css/ionicons.css";
+import "../src/css/styleRubik.css";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 

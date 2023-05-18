00f4113a5bbacc1413bb5649090862123c1f8c7e
Usov_Artem <artemka063@gmail.com>
2023-05-18 16:17:05 +0400

    ScrollToTop added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+// import {Route, Routes} from "react-router-dom";
 import { HomeOne } from "./pages/Home/HomeOne";
 import { HomeTwo } from "./pages/Home/HomeTwo";
 import { Demo } from "./pages/Demo";
@@ -7,19 +7,24 @@ import { About } from "./pages/About";
 import { ComingSoon } from "./pages/ComingSoon";
 import { Contact } from "./pages/Contacts";
 import { NotFound } from "./pages/NotFound";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from '../src/Components/ScrollToTop';
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/kidol_react" element={ <Demo/> }/>
-                <Route path="/HomeOne" element={ <HomeOne/> }/>
-                <Route path="/HomeTwo" element={ <HomeTwo/> }/>
-                <Route path="/About" element={ <About/> }/>
-                <Route path="/NotFound" element={ <NotFound/> }/>
-                <Route path="/ComingSoon" element={ <ComingSoon/> }/>
-                <Route path="/Contact" element={ <Contact/> }/>
-            </Routes>
+            {/* <Router> */}
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/kidol_react" element={ <Demo/> }/>
+                    <Route path="/HomeOne" element={ <HomeOne/> }/>
+                    <Route path="/HomeTwo" element={ <HomeTwo/> }/>
+                    <Route path="/About" element={ <About/> }/>
+                    <Route path="/NotFound" element={ <NotFound/> }/>
+                    <Route path="/ComingSoon" element={ <ComingSoon/> }/>
+                    <Route path="/Contact" element={ <Contact/> }/>
+                </Routes>
+            {/* </Router> */}
         </>
     )
 }
---
+++ src/Components/ScrollToTop.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, [pathname]);
+    
+
+    // useEffect(() => {
+    //     const handleBeforeUnload = (event) => {
+    //       event.preventDefault();
+    //       event.returnValue = '';
+    //       window.scrollTo(0, 0);
+    //     };
+      
+    //     window.addEventListener('beforeunload', handleBeforeUnload);
+      
+    //     return () => {
+    //       window.removeEventListener('beforeunload', handleBeforeUnload);
+    //     };
+    //   }, []);
+    // useEffect(() => {
+    //     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    //       event.preventDefault();
+    //       event.returnValue = '';
+    //       window.scrollTo(0, 0);
+    //     };
+      
+    //     window.addEventListener('beforeunload', handleBeforeUnload);
+      
+    //     return () => {
+    //       window.removeEventListener('beforeunload', handleBeforeUnload);
+    //     };
+    //   }, [pathname]);
+    // useEffect(() => {
+    //   const unlisten = window.addEventListener('beforeunload', () => {
+    //     window.scrollTo(0, 0);
+    //   });
+    //   return () => {
+    //     window.removeEventListener('beforeunload', unlisten);
+    //   };
+    // }, [pathname]);
+  
+    return null;
+  };
+
+export default ScrollToTop;

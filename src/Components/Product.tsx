f846d478717d2003504ce6e82496aeffb6669e65
Usov_Artem <artemka063@gmail.com>
2023-06-13 19:58:07 +0400

    ClassName added

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -8,51 +8,48 @@ import { Link } from 'react-router-dom';
 
 interface ProductProps {
 	product: IProduct;
+	className: string;
 }
 export const ProductArea: IProduct[] = Products.filter(product => product.id < 9);
 
-export function Product({ product }: ProductProps) {
+export function Product({ product, className }: ProductProps) {
 	return (
 		<>
-			<div className='col-lg-3 col-md-4 col-sm-6'>
-				{/* Start Product Item  */}
-				<div className='product-item'>
-					<div className='product-thumb'>
-						<img src={product.image} alt='Image' />
-						<div className='product-action'>
-							<a className='action-quick-view' href='shop-cart.html'>
-								<i className='ion-ios-cart'></i>
-							</a>
-							<a className='action-quick-view' href='javascript:void(0)'>
-								<i className='ion-ios-expand'></i>
-							</a>
-							<a className='action-quick-view' href='shop-wishlist.html'>
-								<i className='ion-ios-heart'></i>
-							</a>
-							<a className='action-quick-view' href='shop-compare.html'>
-								<i className='ion-ios-shuffle'></i>
-							</a>
-						</div>
-					</div>
-					<div className='product-info'>
-						<div className='rating'>
-							<span className='fa fa-star'></span>
-							<span className='fa fa-star'></span>
-							<span className='fa fa-star'></span>
-							<span className='fa fa-star'></span>
-							<span className='fa fa-star'></span>
-						</div>
-						<h4 className='title'>
-							<Link to={product.link}>
-								<a>{product.title}</a>
-							</Link>
-						</h4>
-						<div className='prices'>
-							<span className='price'>{product.price}</span>
-						</div>
+			<div className={`product-item ${className}`}>
+				<div className='product-thumb'>
+					<img src={product.image} />
+					<div className='product-action'>
+						<a className='action-quick-view' href='shop-cart.html'>
+							<i className='ion-ios-cart'></i>
+						</a>
+						<a className='action-quick-view' href='javascript:void(0)'>
+							<i className='ion-ios-expand'></i>
+						</a>
+						<a className='action-quick-view' href='shop-wishlist.html'>
+							<i className='ion-ios-heart'></i>
+						</a>
+						<a className='action-quick-view' href='shop-compare.html'>
+							<i className='ion-ios-shuffle'></i>
+						</a>
 					</div>
 				</div>
-				{/*End Product Item*/}
+				<div className='product-info'>
+					<div className='rating'>
+						<span className='fa fa-star'></span>
+						<span className='fa fa-star'></span>
+						<span className='fa fa-star'></span>
+						<span className='fa fa-star'></span>
+						<span className='fa fa-star'></span>
+					</div>
+					<h4 className='title'>
+						<Link to={product.link}>
+							<a>{product.title}</a>
+						</Link>
+					</h4>
+					<div className='prices'>
+						<span className='price'>{product.price}</span>
+					</div>
+				</div>
 			</div>
 		</>
 	);

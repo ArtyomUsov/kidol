8df96e8b0125a6a6cb5a71d9f00d548ac56e1dbf
Usov_Artem <artemka063@gmail.com>
2023-07-19 16:38:49 +0400

    Carouse update

--- src/Components/Carouse.tsx
+++ src/Components/Carouse.tsx
@@ -51,51 +51,156 @@ export function Carouse() {
 								</button>
 							}
 						>
-							<Carousel.Item>
-								<div className='d-flex'>
-									{Products.slice(activeIndex % Products.length).map((product, index) => (
-										<>
-											<div key={product.id} className='product-item col-12 col-lg-3 col-md-4 col-sm-6 slide-item'>
-												<div className='product-thumb'>
-													<img src={product.image} alt='Image' />
-													<div className='product-action'>
-														<Link to={CartPage} className='action-quick-view'>
-															<i className='ion-ios-cart'></i>
-														</Link>
-														<Link to={''} className='action-quick-view' onClick={handleClick}>
-															<i className='ion-ios-expand'></i>
-														</Link>
-														<Link to={Wishlist} className='action-quick-view'>
-															<i className='ion-ios-heart'></i>
-														</Link>
-														<Link to={Compare} className='action-quick-view'>
-															<i className='ion-ios-shuffle'></i>
-														</Link>
-													</div>
-												</div>
-												<div className='product-info'>
-													<div className='rating'>
-														<span className='fa fa-star'></span>
-														<span className='fa fa-star'></span>
-														<span className='fa fa-star'></span>
-														<span className='fa fa-star'></span>
-														<span className='fa fa-star'></span>
-													</div>
-													<h4 className='title'>
-														<Link to={product.link}>
-															<a>{product.title}</a>
-														</Link>
-													</h4>
-													<div className='prices'>
-														<span className='price'>{product.price}</span>
-													</div>
-												</div>
+							{/* {Products.slice((activeIndex + Products.length) % Products.length, (activeIndex + 4 + Products.length) % Products.length).map((product, index) => ( */}
+							{Products.map((product, index) => (
+								<Carousel.Item key={product.id}><div   className='d-flex'>
+									<div className='product-item col-12 col-lg-3 col-md-4 col-sm-6 slide-item'>
+										<div className='product-thumb'>
+											<img src={product.image} alt='Image' />
+											<div className='product-action'>
+												<Link to={CartPage} className='action-quick-view'>
+													<i className='ion-ios-cart'></i>
+												</Link>
+												<Link to={''} className='action-quick-view' onClick={handleClick}>
+													<i className='ion-ios-expand'></i>
+												</Link>
+												<Link to={Wishlist} className='action-quick-view'>
+													<i className='ion-ios-heart'></i>
+												</Link>
+												<Link to={Compare} className='action-quick-view'>
+													<i className='ion-ios-shuffle'></i>
+												</Link>
 											</div>
-											<div>{ActiveTab && <ProductQuickView setActive={setActiveTab} />}</div>
-										</>
-									))}
-								</div>
-							</Carousel.Item>
+										</div>
+										<div className='product-info'>
+											<div className='rating'>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+											</div>
+											<h4 className='title'>
+												<Link to={product.link}>
+													<a>{product.title}</a>
+												</Link>
+											</h4>
+											<div className='prices'>
+												<span className='price'>{product.price}</span>
+											</div>
+										</div>
+									</div>
+									<div className='product-item col-12 col-lg-3 col-md-4 col-sm-6 slide-item'>
+										<div className='product-thumb'>
+											<img src={product.image} alt='Image' />
+											<div className='product-action'>
+												<Link to={CartPage} className='action-quick-view'>
+													<i className='ion-ios-cart'></i>
+												</Link>
+												<Link to={''} className='action-quick-view' onClick={handleClick}>
+													<i className='ion-ios-expand'></i>
+												</Link>
+												<Link to={Wishlist} className='action-quick-view'>
+													<i className='ion-ios-heart'></i>
+												</Link>
+												<Link to={Compare} className='action-quick-view'>
+													<i className='ion-ios-shuffle'></i>
+												</Link>
+											</div>
+										</div>
+										<div className='product-info'>
+											<div className='rating'>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+											</div>
+											<h4 className='title'>
+												<Link to={product.link}>
+													<a>{product.title}</a>
+												</Link>
+											</h4>
+											<div className='prices'>
+												<span className='price'>{product.price}</span>
+											</div>
+										</div>
+									</div>
+									<div className='product-item col-12 col-lg-3 col-md-4 col-sm-6 slide-item'>
+										<div className='product-thumb'>
+											<img src={product.image} alt='Image' />
+											<div className='product-action'>
+												<Link to={CartPage} className='action-quick-view'>
+													<i className='ion-ios-cart'></i>
+												</Link>
+												<Link to={''} className='action-quick-view' onClick={handleClick}>
+													<i className='ion-ios-expand'></i>
+												</Link>
+												<Link to={Wishlist} className='action-quick-view'>
+													<i className='ion-ios-heart'></i>
+												</Link>
+												<Link to={Compare} className='action-quick-view'>
+													<i className='ion-ios-shuffle'></i>
+												</Link>
+											</div>
+										</div>
+										<div className='product-info'>
+											<div className='rating'>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+											</div>
+											<h4 className='title'>
+												<Link to={product.link}>
+													<a>{product.title}</a>
+												</Link>
+											</h4>
+											<div className='prices'>
+												<span className='price'>{product.price}</span>
+											</div>
+										</div>
+									</div>
+									<div className='product-item col-12 col-lg-3 col-md-4 col-sm-6 slide-item'>
+										<div className='product-thumb'>
+											<img src={product.image} alt='Image' />
+											<div className='product-action'>
+												<Link to={CartPage} className='action-quick-view'>
+													<i className='ion-ios-cart'></i>
+												</Link>
+												<Link to={''} className='action-quick-view' onClick={handleClick}>
+													<i className='ion-ios-expand'></i>
+												</Link>
+												<Link to={Wishlist} className='action-quick-view'>
+													<i className='ion-ios-heart'></i>
+												</Link>
+												<Link to={Compare} className='action-quick-view'>
+													<i className='ion-ios-shuffle'></i>
+												</Link>
+											</div>
+										</div>
+										<div className='product-info'>
+											<div className='rating'>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+												<span className='fa fa-star'></span>
+											</div>
+											<h4 className='title'>
+												<Link to={product.link}>
+													<a>{product.title}</a>
+												</Link>
+											</h4>
+											<div className='prices'>
+												<span className='price'>{product.price}</span>
+											</div>
+										</div>
+									</div>
+									<div>{ActiveTab && <ProductQuickView setActive={setActiveTab} />}</div></div>
+								</Carousel.Item>
+							))}
 						</Carousel>
 					</div>
 				</div>
